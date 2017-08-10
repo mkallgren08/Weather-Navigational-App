a57740f5e75dda2d7176fbb6a64bca25f2f3c247
Keith Kovach <kovachks90@gmail.com>
2017-08-10 13:37:52 -0400

    cleaned up javascript and added comments

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,23 +1,29 @@
+//-----------------Variables------------------------
+
+// Global variables for our start and finish locations
 var startLocation = "";
 var finishLocation = "";
 
+//-----------------Functions-----------------------
 
-function ajaxTest () {
-var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + startLocation + "&destination=" +
+//Function for retrieving ajax data from google directions map api based on user input for start and finish locations
+function googleDirectionApiCall () {
+	var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + startLocation + "&destination=" +
 				 finishLocation + "&key=AIzaSyCI-Q45nsEkZDVBrp2I8NB2cTTqK_hhgrg";
-console.log(queryURL)
-$.ajax({
-	url: queryURL,
-	method: "GET"
-}).done(function(response) {
-console.log(response)
-})
-
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+	console.log(response)
+	})
 }
 
+//On click event for startpages launch function which will log the start location and finish location as variables. 
 $("#launch").on("click", function() {
 	startLocation = $("#startLocation").val().trim()
 	console.log("start Location: " + startLocation)
 	finishLocation = $("#finishLocation").val().trim()
-	ajaxTest()
+	
+	//running ajax command for retrieval of google direction maps api after user input variables have been saved.
+	googleDirectionApiCall()
 })

b96bffd158ce749efa95c2867e94ed798ba063eb
Keith Kovach <kovachks90@gmail.com>
2017-08-10 13:23:56 -0400

    ajax call for google directions api working on submit click

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,23 @@
+var startLocation = "";
+var finishLocation = "";
+
 
 function ajaxTest () {
-var key = "AIzaSyCI-Q45nsEkZDVBrp2I8NB2cTTqK_hhgrg"
-var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key="
-
+var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + startLocation + "&destination=" +
+				 finishLocation + "&key=AIzaSyCI-Q45nsEkZDVBrp2I8NB2cTTqK_hhgrg";
+console.log(queryURL)
 $.ajax({
-	url: queryURL + key,
+	url: queryURL,
 	method: "GET"
 }).done(function(response) {
-	console.log(response)
-})}
+console.log(response)
+})
 
-ajaxTest();
+}
+
+$("#launch").on("click", function() {
+	startLocation = $("#startLocation").val().trim()
+	console.log("start Location: " + startLocation)
+	finishLocation = $("#finishLocation").val().trim()
+	ajaxTest()
+})

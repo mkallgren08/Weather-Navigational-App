2461fd9ccef087f1fa7033dc69d68083dd4a8d1b
Keith Kovach <kovachks90@gmail.com>
2017-08-14 15:06:47 -0400

    updated secondary map.  Still some bugs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,69 @@
 //-----------------Variables------------------------
 
+var map = "";
+var markerArray = [];
+
+//weatherID =response.weather[0].id 
+
+var weatherID = "";
+
+
+//=======================================================================
+// 		RAIN & THUNDERSTORMS
+//=======================================================================
+
+// Rain: For everything 
+var rainIDArray = [500, 501, 502, 503, 520, 521];
+
+// freezing rain
+var freezingRainID = [511];
+
+// rain with hazardous weather
+var hazRainArray = [502, 503, 504, 522, 901, 902, 906];
+
+// Thunderstorms (ts avatars)
+var thunderIDArray = [200, 201, 210, 211, 221, 230, 231, 232];
+
+// Hazardous Thunderstorms (ts avatars)
+var hazthunderArray = [202, 212];
+
+// =====================================================================
+//		OVERCAST
+// =====================================================================
+
+// overcast day for hazardous reasons (odhw avatars)
+var hazOvercastArray = [711, 721, 731, 441, 751, 761, 762, 771, 781, 900, 905];
+
+// general overcast day (od avatars)
+var overcastArray = [520, 804];
+
+// =====================================================================
+////		SNOW
+// =====================================================================
+
+// heavy snow (hs avatar)
+var heavySnowArray = [602, 622];
+
+// hazardous heavy snow
+//hazHeavySnowArray = [] - nothing for now
+
+// snow (sn avatar)
+var snowArray = [600, 601, 611, 612, 620, 621];
+
+// snow with hazards (snhw avatar)
+var hazSnowArray = [615, 616];
+
+// =====================================================================
+//		CLEAR AND CLOUDY DAYS
+// =====================================================================
+
+// clear day
+
+var clearDayArray = [800];
+// scattered clouds
+var scatteredCloudArray = [801, 802, 803]
+
+
 // Global variables for our start and finish locations
 var startLocation = "";
 var finishLocation = "";
@@ -42,9 +106,6 @@ function fireBaseLog() {
 		endState: endState
 	})
 }
-database.ref().on("child_added", function(childSnapshot) {
-	$("#mainTable").append("<tr><td><img width='25px' height='25px' src='assets/images/Avatars/Sunny-Day-Avatars/test.jpg'</td><td>" + childSnapshot.val().startCity + "</td></tr>")
-})
 
 function getSum(total, num) {
     return total + num;
@@ -70,6 +131,8 @@ function googleDirectionApiCall () {
       console.log("Finish Lon: "+ finishLon);
       weatherMapsAPICall(startLat, startLon);
       weatherMapsAPICall(finishLat, finishLon);
+      // initMap(startLat, startLon)
+      // initMap(finishLat, finishLon)
       var tripSteps = Object.keys(response.routes[0].legs[0].steps)
       console.log(tripSteps);
       for ( var i = 0; i < tripSteps.length-1; i++){
@@ -81,6 +144,7 @@ function googleDirectionApiCall () {
             var stepLon = response.routes[0].legs[0].steps[i].end_location.lng;
             console.log("step distance: " + response.routes[0].legs[0].steps[i].distance.value)
             weatherMapsAPICall(stepLat, stepLon)
+            markerMap(response.routes[0].legs[0].steps[i].end_location)
             // This creates an array to calculate the cumulative distance up to this point
                 for (var k = i; k > -1; k--){
                   stepLengthkm = response.routes[0].legs[0].steps[k].distance.value
@@ -112,25 +176,37 @@ function googleMapEmbedCall () {
 	})
 }
 
-//Function for embeding secondary map which will contain data for weather collected along route
-function initMap() {
-
-    var position1 = {lat: 39.9582118, lng: -82.98304879999999};
-    var position2 = {lat: 39.0, lng: -82.58304879999999};
-    var map = new google.maps.Map(document.getElementById('secondaryMap'), {
-      zoom: 8,
-      center: position1
-    });
-    var marker = new google.maps.Marker({
+function markerMap(coordinates) {
+    var position1 = coordinates;
+    // var position = "lat: " +  Latitude +", lng: " + Longitude
+    // position1.append(position)
+    // console.log(position1)
+    var marker = {
       position: position1,
       map: map,
       icon: "assets/images/Avatars/Sunny-Day-Avatars/test.jpg"
-    });
-    var marker2 = new google.maps.Marker({
-      position: position2,
-      map: map
-    });    
+    };
+    console.log("marker: " + marker.position.lat)
+    markerArray.push(marker);
+    console.log("marker array: " + markerArray)
+};
+//Function for embeding secondary map which will contain data for weather collected along route
+function initMap() {
+
+    // var position2 = {lat: 39.0, lng: -82.58304879999999};
+    var map = new google.maps.Map(document.getElementById('secondaryMap'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+        });
+    for (var l = 0; l < markerArray.length; l++) {
+	var markerPip = new google.maps.Marker(markerArray[l])}
+
 }
+    // var marker2 = new google.maps.Marker({
+    //   position: position2,
+    //   map: map
+    // });    
+
 
 //===================================================================================================
                     //                  OPENWEATHERMAPS                        //
@@ -156,6 +232,47 @@ function initMap() {
   // 	var weatherIconCode
   // 	return weather
   // };
+function displayAvatar(shorthand, output){
+	var avatar = $("<img>")
+	var avatarSrc = "./assets/javascript/Avatars/" + shorthand + ".jpg"
+	console.log("Src = " + avatarSrc)
+	avatar.attr("src", avatarSrc)
+	output.append(avatar)
+}
+
+function pickAvatar(output, response){
+	weatherID = response.weather[0].id;
+	if (rainIDArray.indexOf(weatherID) !== -1){
+			displayAvatar("r", output)
+	} else if (freezingRainID.indexOf(weatherID) !== -1){
+			displayAvatar("fr", output)
+	} else if (hazRainArray.indexOf(weatherID) !== -1){
+			displayAvatar("rhw", output)
+	} else if (thunderIDArray.indexOf(weatherID) !== -1){
+			displayAvatar("ts", output)
+	} else if (hazthunderArray.indexOf(weatherID) !== -1){
+			displayAvatar("tshw", output)
+	} else if (overcastArray.indexOf(weatherID) !== -1){
+			displayAvatar("od", output)
+	} else if (hazOvercastArray.indexOf(weatherID) !== -1){
+			displayAvatar("odhw", output)
+	} else if (heavySnowArray.indexOf(weatherID) !== -1){
+			displayAvatar("hs", output)
+	} else if (hazSnowArray.indexOf(weatherID) !== -1){
+			displayAvatar("snhw", output)
+	} else if (snowArray.indexOf(weatherID) !== -1){
+			displayAvatar("sn", output)
+	} else if (clearDayArray.indexOf(weatherID) !== -1){
+			displayAvatar("cs",output)
+	} else if (scatteredCloudArray.indexOf(weatherID) !== -1){
+			displayAvatar("sc", output)
+	} else {
+		var error = $("<p>")
+		error.text("Error!")
+		$(".avatar-display").html(error)
+	}
+};
+
 
   function weatherMapsAPIResults(response/*, /distance*/){
           cityName = "";
@@ -165,17 +282,11 @@ function initMap() {
           // Create CODE HERE to transfer content to HTML
 
           // make a new block for a city's data-chunk
-          var outputBlock = $("<div>")
-          outputBlock.addClass("outputBlock")
-          outputBlock.attr("style = 'border: 1px solid black'")
-
-          var blockDivider = $("<p>")
-          blockDivider.text("=====================================================")
-          outputBlock.append(blockDivider);
+          var outputBlock = $("<tr>")
 
           // write the city data
-          var city = $("<p>")
-          city.text("City: " + response.name)
+          var city = $("<td>")
+          city.text(response.name)
           outputBlock.append(city)
 
           // Write the distance from last step
@@ -185,12 +296,15 @@ function initMap() {
 
 
           // Pull the icon from Open Weather API
-          var weatherIcon = $("<img>")
-          iconID = response.weather[0].icon
-          iconURL = "https://openweathermap.org/img/w/"+ iconID +".png"
-          var iconURL = "https://openweathermap.org/img/w/10d.png"
-          weatherIcon.attr("src", iconURL)
-          outputBlock.append(weatherIcon)
+          var avatarCell = $("<td>")
+          pickAvatar(avatarCell, response);
+
+
+          // iconID = response.weather[0].icon
+          // iconURL = "https://openweathermap.org/img/w/"+ iconID +".png"
+          // var iconURL = "https://openweathermap.org/img/w/10d.png"
+          // weatherIcon.attr("src", iconURL)
+          // outputBlock.append(weatherIcon)
 
           // write the wind data
           var wind = $("<p>");
@@ -213,7 +327,7 @@ function initMap() {
           temperature.append("<p>" + "Temperature: " + degreeF + "F" + "</p>");
           outputBlock.append(temperature);
 
-          $("#dataOutput").append(outputBlock);
+          $("#mainTable").append(outputBlock);
     }
 
 // <<<<<<< HEAD
@@ -223,10 +337,10 @@ function initMap() {
 // >>>>>>> c99f52d21a5bc1acf63231bc20fffae52216dfdf
 //On click event for startpages launch function which will log the start location and finish location as variables. 
 $("#launch").on("click", function() {
-<<<<<<< HEAD
-=======
+// <<<<<<< HEAD
+// =======
 	
->>>>>>> 636ece38b3d0ee8a7c43e64a125ca3e923b1f485
+// >>>>>>> 636ece38b3d0ee8a7c43e64a125ca3e923b1f485
 	startCity = $("#startCity").val().trim();
 	startState = $("#startState").val().trim();
 	endCity = $("#endCity").val().trim();
@@ -234,8 +348,6 @@ $("#launch").on("click", function() {
 	
 	//running ajax command for retrieval of google direction maps api after user input variables have been saved.
 	googleDirectionApiCall();
-
-
 
 	//running ajax command for retreival of google maps embed map
 	googleMapEmbedCall();

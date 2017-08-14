af0ef6f9ce65ed4c09ef61aa37ec9df608564575
unknown <kovachks90@gmail.comgit config --global kovachks90 Keith>
2017-08-14 00:14:58 -0400

    added marker map with fixed markers for testing

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -43,6 +43,7 @@ function googleDirectionApiCall () {
 	})
 }
 
+//Function for embeding map on index.html using the users entered data
 function googleMapEmbedCall () {
 	var queryURLMaps = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyCI-Q45nsEkZDVBrp2I8NB2cTTqK_hhgrg&origin=" + startCity + "," + startState
 					+ "&destination=" + endCity + "," + endState
@@ -54,7 +55,31 @@ function googleMapEmbedCall () {
 		console.log(queryURLMaps)
 		$("#mapLocation").attr("src", queryURLMaps)
 	})
-	
+}
+
+//Function for embeding secondary map which will contain data for weather collected along route
+function initMap() {
+	var icons = {
+		rain: {
+			icon: "assets/images/Avatars/Avatar-with-Rain-Background.jpg"
+		}
+	}
+
+    var position1 = {lat: 39.9582118, lng: -82.98304879999999};
+    var position2 = {lat: 39.0, lng: -82.58304879999999};
+    var map = new google.maps.Map(document.getElementById('secondaryMap'), {
+      zoom: 8,
+      center: position1
+    });
+    var marker = new google.maps.Marker({
+      position: position1,
+      map: map,
+      icon: "assets/images/Avatars/Sunny-Day-Avatars/test.jpg"
+    });
+    var marker2 = new google.maps.Marker({
+      position: position2,
+      map: map
+    });    
 }
 
 //On click event for startpages launch function which will log the start location and finish location as variables. 
@@ -67,11 +92,12 @@ $("#launch").on("click", function() {
 	//running ajax command for retrieval of google direction maps api after user input variables have been saved.
 	googleDirectionApiCall();
 
-
 	//running ajax command for retreival of google maps embed map
 	googleMapEmbedCall();
 
 	//running firebase log to push user data to firebase
 	fireBaseLog();
+
+	$("body").append("<script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDDopEOP1NAJOEi6wHEHABa_qz8Z6Npe_E&callback=initMap'></script>")
 
 })

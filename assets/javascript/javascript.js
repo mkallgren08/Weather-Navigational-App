242d34c7ccc5039cf0f8efa5586e88f59b40d0de
unknown <kovachks90@gmail.comgit config --global kovachks90 Keith>
2017-08-15 20:38:49 -0400

    cleaned up code to be more legible and added comments

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,24 @@
-//-----------------Variables------------------------
+//-----------------Global Variables------------------------
 
 var map = "";
-var markerArray = [];
 
-//weatherID =response.weather[0].id 
+var markerArray = [];
 
 var weatherID = "";
 
+var startLocation = "";
+
+var finishLocation = "";
+
+var cityName= "";
+
+var latitude = "";
+
+var longitude = "";
+
+var stepDistances = []
+
+var stepLength = ""
 
 //=======================================================================
 // 		RAIN & THUNDERSTORMS
@@ -44,9 +56,6 @@ var overcastArray = [520, 804, 701];
 // heavy snow (hs avatar)
 var heavySnowArray = [602, 622];
 
-// hazardous heavy snow
-//hazHeavySnowArray = [] - nothing for now
-
 // snow (sn avatar)
 var snowArray = [600, 601, 611, 612, 620, 621];
 
@@ -58,17 +67,14 @@ var hazSnowArray = [615, 616];
 // =====================================================================
 
 // clear day
-
 var clearDayArray = [800];
 // scattered clouds
 var scatteredCloudArray = [801, 802, 803]
 
-
-// Global variables for our start and finish locations
-var startLocation = "";
-var finishLocation = "";
-
+// =====================================================================
 // Initialize Firebase
+// =====================================================================
+
 var config = {
   apiKey: "AIzaSyAI-jL25Uyj3rF0-402z3aCVMaheImMqQk",
   authDomain: "navigation-weather-app.firebaseapp.com",
@@ -83,21 +89,9 @@ var APIKey = "f29cdafe21624061235bd7d34ec68e05"; // Openweathermap API key
 
 var database = firebase.database();
 
-var cityName= "";
-
-var latitude = "";
-
-var longitude = "";
-
-var stepDistances = []
-
-var stepLength = ""
-
 //-----------------Functions-----------------------
 
-
-
-//Start City, Start State, End City, End State
+//Pushing user input data to Firebase
 function fireBaseLog() {
 	database.ref("Trip Data").push({
 		startCity: startCity,
@@ -107,6 +101,7 @@ function fireBaseLog() {
 	});
 };
 
+//Logging the last 5 searches to the html div
 database.ref("Trip Data").limitToLast(5).on("child_added", function(childSnapshot) {
     console.log("Child Snapshot: " + childSnapshot.val());
     console.log("Start City: " + childSnapshot.val().startCity);
@@ -125,40 +120,31 @@ database.ref("Trip Data").limitToLast(5).on("child_added", function(childSnapsho
     searchHistory.append(startHistory);
     searchHistory.append(endHistory);
     $("#navHistoryData").append(searchHistory)
-
 });
 
+//adding the text to the history data div once clicked by user
 $(document).on("click", ".navHistoryData", function(event){
-        event.preventDefault();
-        var startingData = this.$(".startEntry").text();
-        var result = startingData.split(", ");
-        startCity = result[0];
-        startState = result[1];
-        var endingData = this.$(".endEntry").text();
-        var result2 = endingData.split(", ")
-        endCity = result2[0];
-        endState = result2[1];
-        $("#startCity").append(startCity);
-        $("#startState").append(startState);
-        $("#endCity").append(endCity);
-        $("#endState").append(endState);
-
+    event.preventDefault();
+    var startingData = this.$(".startEntry").text();
+    var result = startingData.split(", ");
+    startCity = result[0];
+    startState = result[1];
+    var endingData = this.$(".endEntry").text();
+    var result2 = endingData.split(", ")
+    endCity = result2[0];
+    endState = result2[1];
+    $("#startCity").append(startCity);
+    $("#startState").append(startState);
+    $("#endCity").append(endCity);
+    $("#endState").append(endState);
 })
+
 
 function getSum(total, num) {
     return total + num;
 }
 
-function weatherMapsAPICallTwo(latitude, longitude/*, distance*/){
-var queryWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=" + APIKey + 
-  "&lat=" + latitude + "&lon=" + longitude;
-  $.ajax({
-    url: queryWeatherURL,
-    method: "GET"
-  }).done(function(response) {
-    weatherMapsAPIIcons(response);
-  });
-};
+//pushing dynamically generated icons to an array for use on the weather map
 var iconArray = [];
 function weatherMapsAPIIcons(response){
         iconID = response.weather[0].icon
@@ -175,50 +161,71 @@ function googleDirectionApiCall () {
 		method: "GET"
 	}).done(function(response) {
 	console.log(response)
-	console.log(response);
-      startLat = response.routes[0].legs[0].start_location.lat;
-      console.log("Start Lat: "+ startLat);
-      startLon = response.routes[0].legs[0].start_location.lng;
-      console.log("Start Lon: "+ startLon);
-      finishLat = response.routes[0].legs[0].end_location.lat;
-      console.log("Finish Lat: "+ finishLat);
-      finishLon = response.routes[0].legs[0].end_location.lng;
-      console.log("Finish Lon: "+ finishLon);
-      weatherMapsAPICall(startLat, startLon);
-      weatherMapsAPICall(finishLat, finishLon);
-      // initMap(startLat, startLon)
-      // initMap(finishLat, finishLon)
-      var tripSteps = Object.keys(response.routes[0].legs[0].steps)
-      console.log(tripSteps);
-            var stepLat = response.routes[0].legs[0].steps[0].end_location.lat;
-            var stepLon = response.routes[0].legs[0].steps[0].end_location.lng;
-            weatherMapsAPICallTwo(stepLat, stepLon)
-            markerMap(response.routes[0].legs[0].steps[0].end_location)
-      for ( var i = 0; i < tripSteps.length-1; i++){
-          if (response.routes[0].legs[0].steps[i].distance.value >= 8000 /*~5 miles*/){
-            //clears the distance array of value:
-            stepDistances = []
-            // Pulls the step-endpoint's latitude and longitude
-            var stepLat = response.routes[0].legs[0].steps[i].end_location.lat;
-            var stepLon = response.routes[0].legs[0].steps[i].end_location.lng;
-            console.log("step distance: " + response.routes[0].legs[0].steps[i].distance.value)
-            weatherMapsAPICall(stepLat, stepLon)
-            weatherMapsAPICallTwo(stepLat, stepLon)
-            markerMap(response.routes[0].legs[0].steps[i].end_location)
-            // This creates an array to calculate the cumulative distance up to this point
-                for (var k = i; k > -1; k--){
-                  stepLengthkm = response.routes[0].legs[0].steps[k].distance.value
-                  var stepLengthmiles = Math.round(stepLengthkm/1609.344)
-                  stepDistances.push(stepLengthmiles)
-                }
-            console.log("Array of step distances: " + stepDistances)
-            //This returns miles
-            cumulativeDistance = stepDistances.reduce(getSum, 0)
-            //cumulativeDistance = (parseInt(cumulativeDistance)/1609.344)
-            console.log("Cumulative distance at this step: " + cumulativeDistance)
-          }
 
-      }
+    //Logging variables which will be used in multiple API calls
+    startLat = response.routes[0].legs[0].start_location.lat;
+    console.log("Start Lat: "+ startLat);
+    startLon = response.routes[0].legs[0].start_location.lng;
+    console.log("Start Lon: "+ startLon);
+    finishLat = response.routes[0].legs[0].end_location.lat;
+    console.log("Finish Lat: "+ finishLat);
+    finishLon = response.routes[0].legs[0].end_location.lng;
+    console.log("Finish Lon: "+ finishLon);
+
+    //Calling btoh OpenWeather API calls
+    weatherMapsAPICall(startLat, startLon);
+    weatherMapsAPICall(finishLat, finishLon);
+    
+    //Logging all trip steps into an object
+    var tripSteps = Object.keys(response.routes[0].legs[0].steps)
+    console.log(tripSteps);
+
+    // Pulls the step-endpoint's latitude and longitude
+    var stepLat = response.routes[0].legs[0].steps[0].end_location.lat;
+    var stepLon = response.routes[0].legs[0].steps[0].end_location.lng;
+    
+    //Calling OpenWeather API to log the start locations weather
+    weatherMapsAPICallTwo(stepLat, stepLon)
+    
+    //Pulls the starting legs lat/lng coordinates
+    markerMap(response.routes[0].legs[0].steps[0].end_location)
+    
+    //Loop iterating through each step
+    for ( var i = 0; i < tripSteps.length-1; i++){
+        
+      //if statement stating that if the steps distance is > 5 miles to then log it
+      if (response.routes[0].legs[0].steps[i].distance.value >= 8000 /*~5 miles*/){
+        
+        //clears the distance array of value:
+        stepDistances = []
+        
+        // Pulls the step-endpoint's latitude and longitude
+        var stepLat = response.routes[0].legs[0].steps[i].end_location.lat;
+        var stepLon = response.routes[0].legs[0].steps[i].end_location.lng;
+        console.log("step distance: " + response.routes[0].legs[0].steps[i].distance.value)
+        
+        //Calling both weather API's
+        weatherMapsAPICall(stepLat, stepLon)
+        weatherMapsAPICallTwo(stepLat, stepLon)
+        
+        //Pulls the location for all steps with a distance greater than 5 miles
+        markerMap(response.routes[0].legs[0].steps[i].end_location)
+
+        // This creates an array to calculate the cumulative distance up to this point.  Used for future plans
+        for (var k = i; k > -1; k--){
+          stepLengthkm = response.routes[0].legs[0].steps[k].distance.value
+          var stepLengthmiles = Math.round(stepLengthkm/1609.344)
+          stepDistances.push(stepLengthmiles)
+        }
+        console.log("Array of step distances: " + stepDistances)
+
+        //This returns miles
+        cumulativeDistance = stepDistances.reduce(getSum, 0)
+
+        //cumulativeDistance = (parseInt(cumulativeDistance)/1609.344)
+        console.log("Cumulative distance at this step: " + cumulativeDistance)
+      } 
+    }
 	})
 }
 
@@ -236,69 +243,63 @@ function googleMapEmbedCall () {
 	})
 }
 
+//Function for creating an array which contains the latitude/longitude for each leg
 function markerMap(coordinates) {
     var position1 = coordinates;
-    // var position = "lat: " +  Latitude +", lng: " + Longitude
-    // position1.append(position)
-    // console.log(position1)
     var marker = {
       position: position1,
       map: map,
     };
-
-    console.log("marker: " + marker.position.lat)
     markerArray.push(marker);
     console.log("marker array: " + markerArray)
 };
+
 //Function for embeding secondary map which will contain data for weather collected along route
 function initMap() {
-
-    // var position2 = {lat: 39.0, lng: -82.58304879999999};
     var map = new google.maps.Map(document.getElementById('secondaryMap'), {
     center: markerArray[0].position,
     zoom: 6
-        });
- 
+    });
+
+  //Loop which will post markers dynamically generated based on the length of the step 
   for (var l = 0; l < markerArray.length; l++) {
 	var markerPip = new google.maps.Marker({
 		position: markerArray[l].position,
 		map: map,
 		icon: iconArray[l]
-		})}
-
+		})};
 }
-    // var marker2 = new google.maps.Marker({
-    //   position: position2,
-    //   map: map
-    // });    
-
-
 
 //===================================================================================================
                     //                  OPENWEATHERMAPS                        //
 //===================================================================================================
 
+//Calling openWeather API and logging the object as a JSON object
+function weatherMapsAPICall(latitude, longitude){
+  var queryWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=" + APIKey + 
+    "&lat=" + latitude + "&lon=" + longitude;
+    $.ajax({
+      url: queryWeatherURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response)
+      weatherMapsAPIResults(response);
+  });
+};
 
-  function weatherMapsAPICall(latitude, longitude/*, distance*/){
-    //var cityName = $("#startLocation").val().trim();
-    // var latitude = $("#startLat").val().trim();
-    // var longitude = $("#startLon").val().trim();
-    //console.log(cityName);
-    var queryWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=" + APIKey + 
-      "&lat=" + latitude + "&lon=" + longitude;
-      $.ajax({
-        url: queryWeatherURL,
-        method: "GET"
-      }).done(function(response) {
-        console.log(response)
-        weatherMapsAPIResults(response/*, distance*/);
-      });
-  };
+//Same as above but used for another purpose
+function weatherMapsAPICallTwo(latitude, longitude){
+var queryWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=" + APIKey + 
+  "&lat=" + latitude + "&lon=" + longitude;
+  $.ajax({
+    url: queryWeatherURL,
+    method: "GET"
+  }).done(function(response) {
+    weatherMapsAPIIcons(response);
+  });
+};
 
-  // function getWeatherIcon(response){
-  // 	var weatherIconCode
-  // 	return weather
-  // };
+//Logic for displaying the avatar in the table
 function displayAvatar(shorthand, output){
 	var avatar = $("<img>")
 	var avatarSrc = "./assets/images/Avatars/" + shorthand + ".jpg"
@@ -310,6 +311,7 @@ function displayAvatar(shorthand, output){
   return output
 }
 
+//Logic for picking the weather to be displayed in the weather table
 function pickAvatar(output, response){
 	weatherID = response.weather[0].id;
 	if (rainIDArray.indexOf(weatherID) !== -1){
@@ -343,66 +345,53 @@ function pickAvatar(output, response){
 	}
 };
 
-  function weatherMapsAPIResults(response/*, /distance*/){
-          cityName = "";
-          // make a new block for a city's data-chunk
-          var outputBlock = $("<tr>")
-          // Pull the icon from Open Weather API
-          var avatarCell = $("<td>")
-          avatarCell.append(pickAvatar(avatarCell, response))
-          //avatarCell.text("Eyyyyy!")
-          //var avatar = pickAvatar(avatarCell, response)
-          outputBlock.append(avatarCell)
-          // write the city data
-          var city = $("<td>");
-          city.text(response.name);
-          outputBlock.append(city);
-          //write the weather data
-          var weather =  $("<td>");
-          weather.text(response.weather[0].description);
-          outputBlock.append(weather);
-          // write the temperature
-          var degreeF = Math.round(((response.main.temp)-273)*1.8 + 32);
-          var temp =  "<td>" + degreeF + " &deg; </td>";
-          outputBlock.append(temp);
-          //write the humidity
-          var humidityStat = $("<td>");
-          humidityStat.text(response.main.humidity + "%");
-          outputBlock.append(humidityStat)
-          // write the wind data
-          var wind = $("<td>");
-          wind.text(Math.round((response.wind.speed)*0.62) + "mph")
-          outputBlock.append(wind);
+function weatherMapsAPIResults(response/*, /distance*/){
+  cityName = "";
 
-          $("#tripWeather").append(outputBlock);
-          // Write the distance from last step
-          // var cumulativeDist = $("<p>")
-          // cumulativeDist.text("Distance from start: " + distance)
-          // outputBlock.append(cumulativeDist)       
+  // make a new block for a city's data-chunk
+  var outputBlock = $("<tr>")
 
+  // Pull the icon from Open Weather API
+  var avatarCell = $("<td>")
+  avatarCell.append(pickAvatar(avatarCell, response))
+  outputBlock.append(avatarCell)
 
-          // iconID = response.weather[0].icon
-          // iconURL = "https://openweathermap.org/img/w/"+ iconID +".png"
-          // var iconURL = "https://openweathermap.org/img/w/10d.png"
-          // weatherIcon.attr("src", iconURL)
-          // outputBlock.append(weatherIcon)
+  // write the city data
+  var city = $("<td>");
+  city.text(response.name);
+  outputBlock.append(city);
 
+  //write the weather data
+  var weather =  $("<td>");
+  weather.text(response.weather[0].description);
+  outputBlock.append(weather);
 
-    }
+  // write the temperature
+  var degreeF = Math.round(((response.main.temp)-273)*1.8 + 32);
+  var temp =  "<td>" + degreeF + " &deg; </td>";
+  outputBlock.append(temp);
 
-// <<<<<<< HEAD
-// =======
+  //write the humidity
+  var humidityStat = $("<td>");
+  humidityStat.text(response.main.humidity + "%");
+  outputBlock.append(humidityStat)
 
+  // write the wind data
+  var wind = $("<td>");
+  wind.text(Math.round((response.wind.speed)*0.62) + "mph")
+  outputBlock.append(wind);
+  $("#tripWeather").append(outputBlock);
+}
+
+//Posting user input locations to the html page
 function postLocation() {
 $("#currentDirections").html("Start Location: " + startCity + ", " + startState + "<br>End Location: " + endCity + ", " + endState)
 }
-// >>>>>>> c99f52d21a5bc1acf63231bc20fffae52216dfdf
+
 //On click event for startpages launch function which will log the start location and finish location as variables. 
 $("#launch").on("click", function() {
-// <<<<<<< HEAD
-// =======
 	
-// >>>>>>> 636ece38b3d0ee8a7c43e64a125ca3e923b1f485
+  //Logging user input and storing it into variables
 	startCity = $("#startCity").val().trim();
 	startState = $("#startState").val().trim();
 	endCity = $("#endCity").val().trim();
@@ -417,11 +406,11 @@ $("#launch").on("click", function() {
 	//running firebase log to push user data to firebase
 	fireBaseLog();
 
+  //Posting user nput locations to html page
   postLocation();
 
+  //Appending the script to the body which will call the map which contains weather information
 	setTimeout(function(){
 		$("body").append("<script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDDopEOP1NAJOEi6wHEHABa_qz8Z6Npe_E&callback=initMap'></script>")}
 		,3000);
-	
-
 })
